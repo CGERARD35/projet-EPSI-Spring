41958898c521e1be47f0cfe92e1000e312f4e228
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-06-02 15:01:18 +0200

    test commit Benjamin

--- src/main/java/projetPOEIspring/poeidata/ProjetPoeiSpringApplication.java
+++ src/main/java/projetPOEIspring/poeidata/ProjetPoeiSpringApplication.java
@@ -8,6 +8,7 @@ public class ProjetPoeiSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetPoeiSpringApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
