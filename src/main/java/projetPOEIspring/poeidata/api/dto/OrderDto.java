8dfb14b2ce9cc749d7a38858f66a885c84d74bad
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-06-10 11:08:50 +0200

    Order model

--- src/main/java/projetPOEIspring/poeidata/api/dto/OrderDto.java
+++ src/main/java/projetPOEIspring/poeidata/api/dto/OrderDto.java
@@ -1,4 +1,36 @@
 package projetPOEIspring.poeidata.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
+
+    private Integer id;
+
+    private Date dateCommande;
+
+    private Double prix;
+
+    private String duree;
+
+    private  String statut;
+
+    private String notes;
+
+    private String clientNom;
+    private String prenomClient;
+    private String companyClient;
+
+    private String nomProduit;
+    private String prixProduit;
+
 }
--- src/main/java/projetPOEIspring/poeidata/models/Order.java
+++ src/main/java/projetPOEIspring/poeidata/models/Order.java
@@ -1,4 +1,44 @@
 package projetPOEIspring.poeidata.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "date_commande", nullable = false)
+    private Date dateCommande;
+
+    @Column(name = "prix", nullable = false)
+    private Double prix;
+
+    @Column(name = "duree", nullable = false)
+    private String duree;
+
+    @Column(name = "statut", nullable = false)
+    private  String statut;
+
+    @Column(name = "notes")
+    private String notes;
+
+//    OneToMany
+//    private List<Client> clients;
+
+//    OneToMany
+//    private List<Produit> produit;
+
+
 }
