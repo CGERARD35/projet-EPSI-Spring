3b2cc3d79c6e5bc2793ae4249e18b2d7798821ba
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-06-10 11:09:05 +0200

    Order service

--- src/main/java/projetPOEIspring/poeidata/services/OrdersService.java
+++ src/main/java/projetPOEIspring/poeidata/services/OrdersService.java
@@ -1,4 +1,14 @@
 package projetPOEIspring.poeidata.services;
 
+import projetPOEIspring.poeidata.models.Order;
+
+import java.util.List;
+
 public interface OrdersService {
+
+    List<Order> getAll();
+    Order getById(Integer id);
+    Order create(Order order);
+    Order update(Order order);
+    void delete(Integer integer);
 }
--- src/main/java/projetPOEIspring/poeidata/services/impl/OrderServiceImpl.java
+++ src/main/java/projetPOEIspring/poeidata/services/impl/OrderServiceImpl.java
@@ -1,7 +1,52 @@
 package projetPOEIspring.poeidata.services.impl;
 
 import org.springframework.stereotype.Service;
+import projetPOEIspring.poeidata.exceptions.UnknownResourceException;
+import projetPOEIspring.poeidata.models.Order;
+import projetPOEIspring.poeidata.repositories.OrderRepository;
+import projetPOEIspring.poeidata.services.OrdersService;
+
+import java.util.List;
 
 @Service
-public class OrderServiceImpl {
+public class OrderServiceImpl implements OrdersService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return this.orderRepository.findAll();
+    }
+
+    @Override
+    public Order getById(Integer id) {
+        return this.orderRepository.findById(id)
+                .orElseThrow(()-> new UnknownResourceException());
+    }
+
+    @Override
+    public Order create(Order order) {
+        order.setId(null);
+        return this.orderRepository.save(order);
+    }
+
+    @Override
+    public Order update(Order order) {
+        Order orderToUpdate = this.getById(order.getId());
+        orderToUpdate.setDateCommande(order.getDateCommande());
+        orderToUpdate.setPrix(order.getPrix());
+        orderToUpdate.setDuree(order.getDuree());
+        orderToUpdate.setStatut(order.getStatut());
+        orderToUpdate.setNotes(order.getNotes());
+        return this.orderRepository.save(orderToUpdate);
+    }
+
+    @Override
+    public void delete(Integer integer) {
+
+    }
 }
