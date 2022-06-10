9eee5929acd5176206909456b6d3be205c7d471a
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-06-10 14:04:02 +0200

    Order model generated value

--- src/main/java/projetPOEIspring/poeidata/models/Order.java
+++ src/main/java/projetPOEIspring/poeidata/models/Order.java
@@ -16,6 +16,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
