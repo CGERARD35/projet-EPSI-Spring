366c66a99de293a32e3e9374bc743edf2a566049
Carl GERARD <carl.gerard@epsi.fr>
2022-06-10 15:56:25 +0200

    Ajout Mapper: client

---
+++ src/main/java/projetPOEIspring/poeidata/mappers/ClientMapper.java
@@ -0,0 +1,40 @@
+package projetPOEIspring.poeidata.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.stereotype.Component;
+import projetPOEIspring.poeidata.api.dto.ClientDto;
+import projetPOEIspring.poeidata.api.dto.OrderDto;
+import projetPOEIspring.poeidata.models.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ClientMapper {
+
+    @Mapping(target = "orders", expression = "java(getOrders(client))")
+    ClientDto mapToClientDto(Client client);
+
+    default List<OrderDto> getOrders(Client client) {
+        List<OrderDto> orders = new ArrayList<>();
+        if (client.getOrders() != null) {
+            orders = client.getOrders().stream()
+                    .map(order -> new OrderDto(
+                            order.getId(),
+                            order.getDateCommande(),
+                            order.getPrix(),
+                            order.getDuree(),
+                            order.getStatut(),
+                            order.getNotes()
+                    )).toList();
+        }
+        return orders;
+    }
+
+
+    Client mapToClient (ClientDto clientDto);
+
+}
