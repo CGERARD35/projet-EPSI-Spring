e4a1ff60c4764794e89e85e5daf473ffc852094d
Carl GERARD <carl.gerard@epsi.fr>
2022-06-10 14:39:21 +0200

    Ajout model: client

---
+++ src/main/java/projetPOEIspring/poeidata/models/Client.java
@@ -0,0 +1,50 @@
+package projetPOEIspring.poeidata.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false)
+    private String prenom;
+
+    @Column(nullable = false)
+    private String societe;
+
+    @Column(nullable = false)
+    private String mail;
+
+    @Column(nullable = false)
+    private String portable;
+
+    @Column(nullable = false)
+    private String fixe;
+
+    @Column(nullable = false)
+    private String statut;
+
+    @Column(nullable = false)
+    private String notes;
+
+    @OneToMany(mappedBy = "client")
+    private Set<Order> orders;
+}
